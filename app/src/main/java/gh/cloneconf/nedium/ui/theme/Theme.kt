72a4de0d51f395bb860eb0c27e0b73d9e1181cce
Clone Conflict <cloneconf@gmail.com>
2021-11-30 22:26:16 +0100

    changed primary color to red

--- app/src/main/java/gh/cloneconf/nedium/ui/theme/Theme.kt
+++ app/src/main/java/gh/cloneconf/nedium/ui/theme/Theme.kt
@@ -7,15 +7,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Red200,
+    primaryVariant = Red700,
+    secondary = Orange200
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200,
+    primary = Red500,
+    primaryVariant = Red700,
+    secondary = Orange200,
 
     /* Other default colors to override
 background = Color.White,
