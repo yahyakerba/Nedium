43c5e6eb7b2fc03efb402c354101c751e05f9643
Clone Conflict <cloneconf@gmail.com>
2021-12-09 16:31:36 +0100

    .

--- app/src/main/java/gh/cloneconf/nedium/model/PostInfo.kt
+++ app/src/main/java/gh/cloneconf/nedium/model/PostInfo.kt
@@ -1,17 +1,7 @@
 package gh.cloneconf.nedium.model
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity
 data class PostInfo (
-    @PrimaryKey
     val id : String,
-
-    @ColumnInfo
     val title : String,
-
-    @ColumnInfo
     val desc : String
 )
